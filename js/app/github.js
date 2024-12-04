ee5667ad2cab65ef94e7084c9d024959e978183e
du-cc <yijun@thedu.cc>
2024-12-04 09:58:49 +0000

    updated arguments for windowmgmt

---
+++ js/app/github.js
@@ -0,0 +1,7 @@
+// APP: Pre-production, redirects to github
+// TODO:
+// - Remove this js file when production
+
+import * as windowMgmt from "../os/window/main.js";
+import * as layer from "../os/window/layer.js";
+
